14b2c24f0c5098b53a45b8a0e0dc89a35209a709
Ivan Kosenkov <bouncer77rus@gmail.com>
2019-10-09 14:02:22 +0300

    Установлен вывод при выборе вина для Русской и Английской локали

--- Head_First/Wine_Advisor/app/src/main/java/com/bouncer77/wine_advisor/MainActivity.java
+++ Head_First/Wine_Advisor/app/src/main/java/com/bouncer77/wine_advisor/MainActivity.java
@@ -10,34 +10,48 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WineExpert expert = new WineExpert();
+    private TextView wineInfo;
+    private Spinner grapeColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Получить ссылку на TextView
+        // findViewById возвращает объект View.
+        wineInfo = (TextView) findViewById(R.id.textView_wine_info);
+        //Получить ссылку на Spinner
+        grapeColor = (Spinner) findViewById(R.id.spinner_grape_color);
     }
 
     public void onClickFindWine(View view) {
-        //Получить ссылку на TextView
-        // findViewById возвращает объект View.
-        TextView wineInfo = (TextView) findViewById(R.id.textView_wine_info);
-
-        //Получить ссылку на Spinner
-        Spinner grapeColor = (Spinner) findViewById(R.id.spinner_grape_color);
-
         //Получить вариант, выбранный в Spinner
         // getSelectedItem возвращает Object.
-        String selectGrapeColor = String.valueOf(grapeColor.getSelectedItem());
+        //String selectGrapeColor = String.valueOf(grapeColor.getSelectedItem());
+        int pos = grapeColor.getSelectedItemPosition();
+        StringBuilder description = getDescriptionByPosition(pos);
+        wineInfo.setText(description);
+    }
 
-        List<String> wineTypeList = expert.getWineType(selectGrapeColor);
-        StringBuilder wineTypeFormatted = new StringBuilder();
-        for (String wineType : wineTypeList) {
-            wineTypeFormatted.append(wineType).append("\n");
+    private StringBuilder getDescriptionByPosition(int position) {
+        //String[] descriptions = getResources().getStringArray(R.array.grape_color);
+        String[] descriptions;
+        if (position == 0) {
+            descriptions = getResources().getStringArray(R.array.type_of_white_wine);
+        } else if (position == 1) {
+            descriptions = getResources().getStringArray(R.array.type_of_red_wine);
+        } else {
+            descriptions = null;
         }
 
-        //Вывести выбранный вариант
-        wineInfo.setText(wineTypeFormatted);
+        if (descriptions == null) {
+            return new StringBuilder("Error: Item of list did not found! Please connect with us!");
+        }
 
+        StringBuilder wineTypeFormatted = new StringBuilder();
+        for (String str : descriptions) {
+            wineTypeFormatted.append(str).append("\n");
+        }
+        return wineTypeFormatted;
     }
 }
--- Head_First/Wine_Advisor/app/src/main/java/com/bouncer77/wine_advisor/WineExpert.java
+++ Head_First/Wine_Advisor/app/src/main/java/com/bouncer77/wine_advisor/WineExpert.java
@@ -5,17 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WineExpert {
-    List<String> getWineType(String grapeColor) {
+    List<String> getWineType(int pos) {
         List<String> gCollor = new ArrayList<>();
-        if (grapeColor.equals("Белый")) {
-            gCollor.add("ШАРДОНЕ");
-            gCollor.add("СОВИНЬОН БЛАН");
-            gCollor.add("РИСЛИНГ");
-        } else {
-            gCollor.add("КАБЕРНЕ СОВИНЬОН");
-            gCollor.add("МЕРЛО");
-            gCollor.add("ПИНО НУАР");
-        }
+//
+//        if (pos == 0) {
+//
+//        }
+//        /*if (grapeColor.equals()) {
+//            gCollor.add("ШАРДОНЕ");
+//            gCollor.add("СОВИНЬОН БЛАН");
+//            gCollor.add("РИСЛИНГ");
+//        } else {
+//            gCollor.add("КАБЕРНЕ СОВИНЬОН");
+//            gCollor.add("МЕРЛО");
+//            gCollor.add("ПИНО НУАР");
+//        }*/
         return gCollor;
     }
+
+
 }
