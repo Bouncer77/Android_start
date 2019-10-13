79c0d87b1f6f8ab4bcf697bc258104631779ccf3
Ivan Kosenkov <bouncer77rus@gmail.com>
2019-10-13 22:47:51 +0300

    update StopWatch refactor code (add constant)

--- StopWatch/app/src/main/java/com/hello/stopwatch/MainActivity.java
+++ StopWatch/app/src/main/java/com/hello/stopwatch/MainActivity.java
@@ -1,11 +1,9 @@
 package com.hello.stopwatch;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,10 +13,17 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textViewTimer;
 
+
+    public static final String ISRUN = "isRunning";
+    public static final String WASRUN = "wasRunning";
+    public static final String SEC = "seconds";
+    public static final String MSEC = "milliseconds";
+    public static final int MSECINCREMENT = 125;
+
     private int seconds = 0;
     private int milliseconds_x_125 = 0;
-    private boolean isRunning = false;
-    private boolean wasRunning = false;
+    private boolean isRunning = false; // секундомер работает?
+    private boolean wasRunning = false; // был запущен? (для смены оринтации экрана)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +31,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textViewTimer = findViewById(R.id.textViewTimer);
         if (savedInstanceState != null) {
-            seconds = savedInstanceState.getInt("seconds");
-            isRunning = savedInstanceState.getBoolean("isRunning");
-            wasRunning = savedInstanceState.getBoolean("wasRunning");
+            seconds = savedInstanceState.getInt(SEC);
+            milliseconds_x_125 = savedInstanceState.getInt(MSEC);
+            isRunning = savedInstanceState.getBoolean(ISRUN);
+            wasRunning = savedInstanceState.getBoolean(WASRUN);
         }
         runTimer();
     }
@@ -46,25 +52,13 @@ public class MainActivity extends AppCompatActivity {
         isRunning = wasRunning;
     }
 
-    /*@Override
-    protected void onStop() {
-        super.onStop();
-        wasRunning = isRunning;
-        isRunning = false;
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        isRunning = wasRunning;
-    }*/
-
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("seconds", seconds);
-        outState.putBoolean("isRunning", isRunning);
-        outState.putBoolean("wasRunning", wasRunning);
+        outState.putInt(SEC, seconds);
+        outState.putInt(MSEC, milliseconds_x_125);
+        outState.putBoolean(ISRUN, isRunning);
+        outState.putBoolean(WASRUN, wasRunning);
     }
 
     public void onClickStartTimer(View view) {
@@ -96,14 +90,14 @@ public class MainActivity extends AppCompatActivity {
                 textViewTimer.setText(time);
 
                 if(isRunning) {
-                    milliseconds_x_125 += 125;
+                    milliseconds_x_125 += MSECINCREMENT;
                     if (milliseconds_x_125 >= 1000) {
                         ++seconds;
                         milliseconds_x_125 = 0;
                     }
 
                 }
-                handler.postDelayed(this, 125);
+                handler.postDelayed(this, MSECINCREMENT);
             }
         });
     }
