5f53637a396e092377c11360fb9a515e8a29d261
Ivan Kosenkov <bouncer77rus@gmail.com>
2019-11-06 11:27:10 +0300

    Приложение для работы с фрагментами

---
+++ FragmentSA/app/src/main/java/com/hello/fragmentsa/Fragment1.java
@@ -0,0 +1,25 @@
+package com.hello.fragmentsa;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+
+    final String LOG_TAG = "myLog";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "Fragment1 onCreateView");
+        return inflater.inflate(R.layout.fragment1, null);
+    }
+}
---
+++ FragmentSA/app/src/main/java/com/hello/fragmentsa/Fragment2.java
@@ -0,0 +1,25 @@
+package com.hello.fragmentsa;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment2 extends Fragment {
+
+    final String LOG_TAG = "myLog";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "Fragment2 onCreateView");
+        return inflater.inflate(R.layout.fragment2, null);
+    }
+}
