c5103717c01ddf4dd618918f7e7a3046de89bb55
Ivan Kosenkov <bouncer77rus@gmail.com>
2019-11-06 11:51:58 +0300

    Добавленна перегрузка методов жизненого цикла для фрагментов и главной активности

--- FragmentSA/app/src/main/java/com/hello/fragmentsa/Fragment2.java
+++ FragmentSA/app/src/main/java/com/hello/fragmentsa/Fragment2.java
@@ -1,5 +1,7 @@
 package com.hello.fragmentsa;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,8 +14,21 @@ import androidx.fragment.app.Fragment;
 
 public class Fragment2 extends Fragment {
 
-    final String LOG_TAG = "myLog";
+    private final String LOG_TAG = "myLog";
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d(LOG_TAG, "Fragment2 onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "Fragment2 onCreate");
+    }
+
+    @SuppressLint("InflateParams")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -21,5 +36,53 @@ public class Fragment2 extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         Log.d(LOG_TAG, "Fragment2 onCreateView");
         return inflater.inflate(R.layout.fragment2, null);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(LOG_TAG, "Fragment2 onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "Fragment2 onStart");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "Fragment2 onPause");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "Fragment2 onResume");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "Fragment2 onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(LOG_TAG, "Fragment2 onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "Fragment2 onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(LOG_TAG, "Fragment2 onDetach");
     }
 }
--- FragmentSA/app/src/main/java/com/hello/fragmentsa/MainActivity.java
+++ FragmentSA/app/src/main/java/com/hello/fragmentsa/MainActivity.java
@@ -3,12 +3,51 @@ package com.hello.fragmentsa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "MainActiviti onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "MainActiviti onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "MainActiviti onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "MainActiviti onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "MainActiviti onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "MainActiviti onDestroy");
     }
 }
