468e63b60731e94ecb822dfba5ee97c9161eef2a
Ivan Kosenkov <bouncer77rus@gmail.com>
2019-11-14 00:08:19 +0300

    Добавлено меню

--- BitsAndPizza/app/src/main/java/com/hello/bitsandpizza/MainActivity.java
+++ BitsAndPizza/app/src/main/java/com/hello/bitsandpizza/MainActivity.java
@@ -3,11 +3,15 @@ package com.hello.bitsandpizza;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
 
 import static androidx.fragment.app.FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
 
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FragmentPagerAdapter pagerAdapter =
                 new FragmentPagerAdapter(getSupportFragmentManager(), BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
@@ -43,6 +50,27 @@ public class MainActivity extends AppCompatActivity {
                 };
         ViewPager pager = findViewById(R.id.pager);
         pager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Заполнение меню; элементы действий добавляются на пнаель приложения
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_create_order:
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "This is a message displayed in a Toast",
+                        Toast.LENGTH_SHORT);
+                toast.show();
+                return true;
+            default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 }
 
