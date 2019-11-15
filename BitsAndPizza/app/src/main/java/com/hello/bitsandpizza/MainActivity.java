72071a5bf77c74d76c058fda781fe429726d7a76
Ivan Kosenkov <bouncer77rus@gmail.com>
2019-11-15 18:05:58 +0300

    Обнавлена панель инструментов

--- BitsAndPizza/app/src/main/java/com/hello/bitsandpizza/MainActivity.java
+++ BitsAndPizza/app/src/main/java/com/hello/bitsandpizza/MainActivity.java
@@ -25,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
         SectionPagerAdapter sectionPagerAdapter =
                 new SectionPagerAdapter(
                         this,
