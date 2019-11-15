132296dd898a4866fe4cbc4fdd50a84abd4cfd9e
Ivan Kosenkov <bouncer77rus@gmail.com>
2019-11-15 20:15:44 +0300

    Добавлена Fab кнопка и snackbar

--- BitsAndPizza/app/src/main/java/com/hello/bitsandpizza/OrderActivity.java
+++ BitsAndPizza/app/src/main/java/com/hello/bitsandpizza/OrderActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -19,4 +23,25 @@ public class OrderActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
+
+    public void onClickDone(View view) {
+        //Код, который выполняется при щелчке на FAB-кнопке
+        CharSequence text = "Your order has been updated";
+        Snackbar snackbar = Snackbar.make(
+                findViewById(R.id.coordinator),
+                text,
+                Snackbar.LENGTH_SHORT
+        );
+        snackbar.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(OrderActivity.this,
+                        "Undone!",
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        snackbar.show();
+    }
+
 }
